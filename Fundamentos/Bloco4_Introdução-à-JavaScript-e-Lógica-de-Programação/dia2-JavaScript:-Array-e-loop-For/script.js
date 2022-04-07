ee8d88b6c643ee6180a490eeca71f7b52d435e0c
Gabriel <gabrielribss29@gmail.com>
2022-04-07 18:59:09 -0300

    Calculando e imprimindo a média aritmética dos valores

--- Fundamentos/Bloco4_Introdução-à-JavaScript-e-Lógica-de-Programação/dia2-JavaScript:-Array-e-loop-For/script.js
+++ Fundamentos/Bloco4_Introdução-à-JavaScript-e-Lógica-de-Programação/dia2-JavaScript:-Array-e-loop-For/script.js
@@ -1,8 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
+let media = 0;
 
 for(let index of numbers){
     soma += index;
 }
 
-console.log(soma);
+media = soma / numbers.length;
+
+console.log(media);
