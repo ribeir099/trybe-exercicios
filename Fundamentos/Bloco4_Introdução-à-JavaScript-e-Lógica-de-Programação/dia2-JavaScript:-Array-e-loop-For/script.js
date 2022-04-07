b1cfcb2398a8619341bcb855dde09e108f1423cd
Gabriel <gabrielribss29@gmail.com>
2022-04-07 19:02:07 -0300

    Armazenando o maior valor do array e imprimindo-o

--- Fundamentos/Bloco4_Introdução-à-JavaScript-e-Lógica-de-Programação/dia2-JavaScript:-Array-e-loop-For/script.js
+++ Fundamentos/Bloco4_Introdução-à-JavaScript-e-Lógica-de-Programação/dia2-JavaScript:-Array-e-loop-For/script.js
@@ -1,11 +1,15 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 let media = 0;
+let maior = 0;
 
 for(let index of numbers){
-    soma += index;
+    //soma += index;
+    if(index > maior){
+        maior = index;
+    }
 }
 
-media = soma / numbers.length;
+//media = soma / numbers.length;
 
-console.log(media);
+console.log(maior);
