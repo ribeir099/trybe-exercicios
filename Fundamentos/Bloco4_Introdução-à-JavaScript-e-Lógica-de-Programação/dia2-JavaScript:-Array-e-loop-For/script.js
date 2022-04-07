6d5c0a4a75e77eaa13dccbeac122be4faac360f7
Gabriel <gabrielribss29@gmail.com>
2022-04-07 19:14:10 -0300

    Armazenando 25 valores em um array utilizando um for

--- Fundamentos/Bloco4_Introdução-à-JavaScript-e-Lógica-de-Programação/dia2-JavaScript:-Array-e-loop-For/script.js
+++ Fundamentos/Bloco4_Introdução-à-JavaScript-e-Lógica-de-Programação/dia2-JavaScript:-Array-e-loop-For/script.js
@@ -1,20 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
-let media = 0;
-let menor = 10000000;
+let array = [];
 
-for(let index of numbers){
-    //soma += index;
-    if(index < menor){
-        menor = index;
-    }
+for(let index = 0; index < 25; index += 1){
+    array.push(index + 1);
 }
 
-console.log(menor);
-
-// if(impar === 0){
-//     console.log("nenhum valor ímpar encontrado");
-// }else{
-//     console.log(impar,"número(s) impar(es)");
-// }
+console.log(array);
 
