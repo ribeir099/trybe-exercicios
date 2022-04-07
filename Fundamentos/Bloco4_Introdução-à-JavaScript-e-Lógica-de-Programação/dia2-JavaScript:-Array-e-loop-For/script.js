ce86b9d6bb01bf06308a353e314f94b32415f3f3
Gabriel <gabrielribss29@gmail.com>
2022-04-07 19:10:42 -0300

    Calculando e imprimindo o menor valor do array

--- Fundamentos/Bloco4_Introdução-à-JavaScript-e-Lógica-de-Programação/dia2-JavaScript:-Array-e-loop-For/script.js
+++ Fundamentos/Bloco4_Introdução-à-JavaScript-e-Lógica-de-Programação/dia2-JavaScript:-Array-e-loop-For/script.js
@@ -1,18 +1,20 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 let media = 0;
-let impar = 0;
+let menor = 10000000;
 
 for(let index of numbers){
     //soma += index;
-    if((index % 2) > 0){
-        impar += 1;
+    if(index < menor){
+        menor = index;
     }
 }
 
-if(impar === 0){
-    console.log("nenhum valor ímpar encontrado");
-}else{
-    console.log(impar,"número(s) impar(es)");
-}
+console.log(menor);
+
+// if(impar === 0){
+//     console.log("nenhum valor ímpar encontrado");
+// }else{
+//     console.log(impar,"número(s) impar(es)");
+// }
 
