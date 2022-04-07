cd3358975fe43ec208cfa7c7024706e47c303b5f
Gabriel <gabrielribss29@gmail.com>
2022-04-07 19:08:47 -0300

    Calculando e imprimindo a quantidade de números impares no array

--- Fundamentos/Bloco4_Introdução-à-JavaScript-e-Lógica-de-Programação/dia2-JavaScript:-Array-e-loop-For/script.js
+++ Fundamentos/Bloco4_Introdução-à-JavaScript-e-Lógica-de-Programação/dia2-JavaScript:-Array-e-loop-For/script.js
@@ -1,15 +1,18 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 let media = 0;
-let maior = 0;
+let impar = 0;
 
 for(let index of numbers){
     //soma += index;
-    if(index > maior){
-        maior = index;
+    if((index % 2) > 0){
+        impar += 1;
     }
 }
 
-//media = soma / numbers.length;
+if(impar === 0){
+    console.log("nenhum valor ímpar encontrado");
+}else{
+    console.log(impar,"número(s) impar(es)");
+}
 
-console.log(maior);
