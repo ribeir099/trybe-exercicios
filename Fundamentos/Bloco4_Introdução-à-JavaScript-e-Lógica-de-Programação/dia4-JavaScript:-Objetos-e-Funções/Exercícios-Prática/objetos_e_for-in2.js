a460fc084b584911fcd7c5e479ce4fcfd32ba278
Gabriel <gabrielribss29@gmail.com>
2022-04-11 19:15:27 -0300

    Manipulando e imprimindo o objeto

--- Fundamentos/Bloco4_Introdução-à-JavaScript-e-Lógica-de-Programação/dia4-JavaScript:-Objetos-e-Funções/Exercícios-Prática/objetos_e_for-in2.js
+++ Fundamentos/Bloco4_Introdução-à-JavaScript-e-Lógica-de-Programação/dia4-JavaScript:-Objetos-e-Funções/Exercícios-Prática/objetos_e_for-in2.js
@@ -16,4 +16,6 @@ let leitor = {
     ],
   };
 
-console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama '" + leitor.livrosFavoritos[0].titulo + "'");
+// console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama '" + leitor.livrosFavoritos[0].titulo + "'");
+
+console.log(leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos.");
