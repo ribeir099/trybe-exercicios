0952bd40bb4a5f014e5406ae16b2ca5a5266809e
Gabriel <gabrielribss29@gmail.com>
2022-04-11 20:29:33 -0300

    Criação de um função que retorna o índice do número de maior valor dentro de um array

--- Fundamentos/Bloco4_Introdução-à-JavaScript-e-Lógica-de-Programação/dia4-JavaScript:-Objetos-e-Funções/Exercícios-Prática/funções.js
+++ Fundamentos/Bloco4_Introdução-à-JavaScript-e-Lógica-de-Programação/dia4-JavaScript:-Objetos-e-Funções/Exercícios-Prática/funções.js
@@ -1,20 +1,41 @@
-function verificaPalindrome(string){
-    let inString = '';
+// function verificaPalindrome(string){
+//     let inString = '';
 
-    for(let index = 1; index <= string.length; index += 1){
-        inString += string[string.length - index];
-    }
+//     for(let index = 1; index <= string.length; index += 1){
+//         inString += string[string.length - index];
+//     }
     
-    if(string === inString){
-        return true;
+//     if(string === inString){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+// let palindromo;
+
+// palindromo = verificaPalindrome('desenvolvimento');
+
+// console.log(palindromo);
+
+function maior(array){
+    let maior = -1;
+    let indice;
+
+    for(let index = 0; index < array.length; index += 1){
+        if(array[index] > maior){
+            maior = array[index];
+            indice = index;
+        }
     }
-    else{
-        return false;
-    }
+
+    return indice;
 }
 
-let palindromo;
+let num = [2, 3, 6, 7, 10, 1];
+let indice;
 
-palindromo = verificaPalindrome('desenvolvimento');
+indice = maior(num);
 
-console.log(palindromo);
+console.log(indice);
