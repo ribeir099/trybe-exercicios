aa5e9c2416db7812cb6f5ae21bd6b2bb31c897ac
Gabriel <gabrielribss29@gmail.com>
2022-04-11 18:43:18 -0300

    Imprimindo os valores do objeto utilizando for in

--- Fundamentos/Bloco4_Introdução-à-JavaScript-e-Lógica-de-Programação/dia4-JavaScript:-Objetos-e-Funções/Exercícios-Prática/objetos_e_for-in.js
+++ Fundamentos/Bloco4_Introdução-à-JavaScript-e-Lógica-de-Programação/dia4-JavaScript:-Objetos-e-Funções/Exercícios-Prática/objetos_e_for-in.js
@@ -10,6 +10,10 @@ info.recorrente = 'Sim';
 
 // console.log(info);
 
+// for(let key in info){
+//     console.log(key);
+// }
+
 for(let key in info){
-    console.log(key);
+    console.log(info[key]);
 }
